34ebeaeba4fb261893eb440e5ca2c34459a704c4
DokHolliday <jasimurdock@gmail.com>
2025-06-09 00:01:22 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,40 +1,45 @@
+// utils.js
+
 const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 const moment = require('moment');
 
+/**
+ * Deep copy of input data
+ */
 function copy(input_data) {
-    return JSON.parse(JSON.stringify(input_data));
+	return JSON.parse(JSON.stringify(input_data));
 }
 
-function get_secure_random_string(bytes_length) {
-    const validChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    let array = crypto.randomBytes(bytes_length);
-    array = array.map(x => validChars.charCodeAt(x % validChars.length));
-    const random_string = String.fromCharCode.apply(null, array);
-    return random_string;
+/**
+ * Generate secure random alphanumeric string
+ */
+function get_secure_random_string(bytes_length = 32) {
+	const validChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+	const random = crypto.randomBytes(bytes_length);
+	const mapped = Array.from(random).map(x => validChars.charCodeAt(x % validChars.length));
+	return String.fromCharCode(...mapped);
 }
 
-async function get_hashed_password(password) {
-	// If no environment variable is set, default
-	// to doing 10 rounds.
-	const bcrypt_rounds = process.env.BCRYPT_ROUNDS ? parseInt(process.env.BCRYPT_ROUNDS) : 10;
-
-	return bcrypt.hash(
-		password,
-		bcrypt_rounds
-	);
+/**
+ * Return bcrypt hash of input password
+ */
+async function get_hashed_password(password, saltRounds = parseInt(process.env.BCRYPT_ROUNDS || '10')) {
+	return bcrypt.hash(password, saltRounds);
 }
 
+/**
+ * Log with readable timestamp
+ */
 function logit(input_string) {
-    const datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
-    // Add spacer unless it starts with a `[`
-    const spacer = input_string.startsWith('[') ? '' : ' ';
-    console.log(`[${datetime}]${spacer}${input_string.trim()}`);
+	const datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
+	const spacer = input_string.startsWith('[') ? '' : ' ';
+	console.log(`[${datetime}]${spacer}${input_string.trim()}`);
 }
 
 module.exports = {
-    copy: copy,
-    get_secure_random_string: get_secure_random_string,
-    get_hashed_password: get_hashed_password,
-    logit: logit
+	copy,
+	get_secure_random_string,
+	get_hashed_password,
+	logit
 };
